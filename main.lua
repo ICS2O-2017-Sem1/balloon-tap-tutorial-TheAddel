e27aef9e5b7dc1eec544ca4788044d249e8b2d1f
MrZebarth <kris.zebarth@ocsb.ca>
2017-09-19 11:16:17 -0400

    First Day

--- main.lua
+++ main.lua
@@ -4,4 +4,6 @@
 --
 -----------------------------------------------------------------------------------------
 
--- Your code here
+local background = display.newImageRect("background.png",360,570)
+background.x=display.contentCenterX
+background.y=display.contentCenterY
